88f76ef737f836bc8d5360e0ab65b3d11987d096
Drew Russell <Drew.Russell@rubrik.com>
2019-01-29 13:27:47 -0600

    initial sync

---
+++ README.md
@@ -0,0 +1,25 @@
+# Rubrik Build `priority-p0` Issues
+
+`priority-p0` issues corresponds to all customer facing issues and requests
+
+## Configuration
+
+`go get github.com/google/go-github/github`
+
+## Usage
+
+`go run main.go`
+
+
+## Example Ouput
+```
+rubrik-sdk-for-powershell
+- get-Rubriksla doesn't work with v2 API
+
+rubrik-modules-for-ansible
+- Support recovery of objects via Ansible module
+- Add ability to manage EC2 native protection
+
+rubrik-provider-for-terraform
+- AWS EC2 Instances Export
+```
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/google/go-github/github"
+)
+
+func main() {
+
+	ctx := context.Background()
+
+	// Authenticated GitHub API Connection
+	// githubToken := os.Getenv("GITHUB_AUTH")
+	// // Authentication configuration
+	// ts := oauth2.StaticTokenSource(
+	// 	&oauth2.Token{AccessToken: githubToken},
+	// )
+	// tc := oauth2.NewClient(ctx, ts)
+
+	// client := github.NewClient(tc)
+
+	client := github.NewClient(nil)
+	// Repository sort options -- Per Page 30 = default of GitHub UI
+	repoOpt := &github.RepositoryListByOrgOptions{Type: "public", ListOptions: github.ListOptions{PerPage: 30}}
+
+	// Issue Options
+	issueOpt := &github.IssueListByRepoOptions{Labels: []string{"priority-p0"}, State: "open"}
+
+	githubOrganization := "rubrikinc"
+
+	// Create an array to store the pagination results
+	var allRepos []*github.Repository
+	for {
+		repos, resp, err := client.Repositories.ListByOrg(ctx, githubOrganization, repoOpt)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		allRepos = append(allRepos, repos...)
+		if resp.NextPage == 0 {
+			break
+		}
+
+		repoOpt.Page = resp.NextPage
+
+	}
+
+	for _, repo := range allRepos {
+		repoName := repo.Name
+
+		issues, _, err := client.Issues.ListByRepo(ctx, "rubrikinc", *repoName, issueOpt)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if len(issues) != 0 {
+			fmt.Printf("\n%s\n", *repoName)
+
+		}
+
+		for _, issue := range issues {
+			issueTitle := issue.Title
+			fmt.Printf("- %s\n", *issueTitle)
+
+		}
+
+	}
+
+}
